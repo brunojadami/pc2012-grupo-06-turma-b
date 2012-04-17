ba6238fc55ae44ab895ee3bef4d353d67fea2bdb
lucasjadami <lucasjadami@9360849f-9656-102a-9faf-d73fd754741a>
2012-04-17 20:11:06 +0000

    Improved the palindrome script again.

--- project-02/openmp/src/palindrome.sh
+++ project-02/openmp/src/palindrome.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 for ((threads = 2; threads <= 20; threads++))
 do
-	for ((size = 1; size <= 40; size += 5))
+	for ((size = 1; size <= 41; size += 5))
 	do
 		make -s N_THREADS=$threads BLOCK_SIZE=$size
-		/usr/bin/time -f "$threads $size %e" ./main < small.in
+		/usr/bin/time -f "$threads $size %e" -o palindrome.points -a ./main < small.in
+		echo "Done (threads/size): $threads $size"
 	done
 done
