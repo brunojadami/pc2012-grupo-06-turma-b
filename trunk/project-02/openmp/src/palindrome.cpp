80a50601bd5a92bc47690b88e240e077db92fd4c
lucasjadami <lucasjadami@9360849f-9656-102a-9faf-d73fd754741a>
2012-04-17 19:43:40 +0000

    Added some new params to the palindrome files and changed some algorithm structures.

--- trunk/project-02/openmp/src/palindrome.cpp
+++ trunk/project-02/openmp/src/palindrome.cpp
@@ -16,15 +16,15 @@ int primesCount = 0;
  */
 void updatePalindromesCount(char* word, int s, bool isPhrase)
 {
-	bool palindrome = true;
 	int i;
 	int sum = 0;
+	bool palindrome = true;
 	
-	#pragma omp parallel for reduction(+:sum), reduction(&&:palindrome)
+	#pragma omp parallel for num_threads(N_THREADS) schedule(dynamic, BLOCK_SIZE) reduction(+:sum), reduction(&&:palindrome)
 	for (i = 0; i < s / 2; ++i)
 	{
-		palindrome &= word[i] == word[s - i - 1];
 		sum += word[i] + word[s - i - 1];
+		palindrome &= word[i] == word[s - i - 1];
 	}
 	
 	if (isPhrase)
