e504e509fdcd5feb73594ec8c82080ddd7596561
lucasjadami <lucasjadami@9360849f-9656-102a-9faf-d73fd754741a>
2012-04-29 22:37:51 +0000

    Added docs; set the mpi base.

--- project-02/mpi/src/sieve.cpp
+++ project-02/mpi/src/sieve.cpp
@@ -13,21 +13,24 @@ void sieveMaster()
 		MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 		MPI_Recv(&buffer, status._count, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 		
-		if (status.MPI_SOURCE == MAIN_MASTER_RANK)
+		if (status.MPI_TAG == TAG_KILL && status.MPI_SOURCE == MAIN_MASTER_RANK)
 		{
 			finish = true;
 		}
-		else if (status.MPI_TAG == TAG_PRIME_QUESTION)
+		else if (status.MPI_TAG == TAG_PRIME_QUESTION && status.MPI_SOURCE >= PALINDROME_SLAVES_RANK_START && status.MPI_SOURCE <
+			PALINDROME_SLAVES_RANK_START + PALINDROME_SLAVES_COUNT)
 		{
 			buffer[0] = 1;
-			MPI_Send(buffer, 1, MPI_CHAR, status.MPI_SOURCE, TAG_PRIME_QUESTION, MPI_COMM_WORLD);
+			MPI_Send(buffer, 1, MPI_CHAR, status.MPI_SOURCE, TAG_PRIME_ANSWER, MPI_COMM_WORLD);
 		}
 	}
 	
 	for (int i = SIEVE_SLAVES_RANK_START; i < SIEVE_SLAVES_RANK_START + SIEVE_SLAVES_COUNT; ++i)
 	{
-		MPI_Send(buffer, 1, MPI_CHAR, i, TAG_FINISH, MPI_COMM_WORLD);
+		MPI_Send(buffer, 1, MPI_CHAR, i, TAG_KILL, MPI_COMM_WORLD);
 	}
+	
+	MPI_Send(buffer, 1, MPI_CHAR, MAIN_MASTER_RANK, TAG_SIEVE_FINISHED, MPI_COMM_WORLD);
 }
 
 void sieveSlave()
@@ -41,7 +44,7 @@ void sieveSlave()
 		MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 		MPI_Recv(&buffer, status._count, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 		
-		if (status.MPI_SOURCE == PALINDROME_MASTER_RANK && status.MPI_TAG == TAG_FINISH)
+		if (status.MPI_TAG == TAG_KILL && status.MPI_SOURCE == SIEVE_MASTER_RANK)
 		{
 			finish = true;
 		}
